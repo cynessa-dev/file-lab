d3c3b156abec6e22c04a51145e547a191e5b02af
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-09-14 20:38:15 +0800

    ✨ feat(landing): add initial design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <section id="hero">
+      <h1>FileLab Tagline goes here, can't think of anything just yet.</h1>
+      <p>
+        FileLab brief introduction goes here, it should be clear and concise to explain what the
+        service does.
+      </p>
+    </section>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  margin: auto 0;
+}
+
+p {
+  max-width: 20rem;
+}
+</style>
